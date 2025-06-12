45394055596650daca4761dd1caa511474658c60
John Deivid Sanchez Hoyos <johndeivid202@gmail.com>
2025-06-12 10:43:17 -0500

    Prueba de guardado de usuario (solo backend)

--- backend/proyecto_alquiler_vehiculos/src/main/java/com/example/demo/controlador/UsuarioControlador.java
+++ backend/proyecto_alquiler_vehiculos/src/main/java/com/example/demo/controlador/UsuarioControlador.java
@@ -1,5 +1,37 @@
 package com.example.demo.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.AlquilerVehiculosApplication;
+import com.example.demo.modelo.Usuario;
+import com.example.demo.repositorio.RepositorioUsuario;
+
+@RestController
+
+@RequestMapping("/usuario")
+
 public class UsuarioControlador {
+ 
+	    private final AlquilerVehiculosApplication alquilerVehiculosApplication;
+        	
+		@Autowired
+		private RepositorioUsuario repositorio;
+		
+		UsuarioControlador(AlquilerVehiculosApplication alquilerVehiculosApplication) {
+	        this.alquilerVehiculosApplication = alquilerVehiculosApplication;
+	    }
+		
+		@PostMapping("/guardarUsuario")
+		public Usuario guardarUsuario(@RequestBody Usuario u){
+		    return this.repositorio.save(u);
+		}
 
 }
